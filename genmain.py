600b2c7929d868bd8512bcad9a2b524755c463ee
spec20000 <cssa20032@gmail.com>
2016-06-25 23:52:42 -0700

    impoved main.cpp

--- genmain.py
+++ genmain.py
@@ -1,9 +1,8 @@
 mn = open ('main.cpp','w')
-code = """#include <iostream>
-//using namespace std;
+code = """
+#include <iostream>
+#include <stdlib.h>
 using std::cout;
-using std::cin;
-using std::endl;
 
 int max(int a, int b)
 {
@@ -14,9 +13,11 @@ int max(int a, int b)
 int main(int argc, char** argv)
 {
    int x,y;
-   cout << "Enter the first value: "; cin>>x;
-   cout << "Enter the second value: "; cin>>y;
-   cout << max(x,y);
+   if(argc<3){
+        cout << "Usage: a.exe v1 v2 where Vx are integers\n";
+        return -1;
+    }
+   cout<<max(atoi(argv[1]),atoi(argv[2]));
    return 0;
 }
 """
